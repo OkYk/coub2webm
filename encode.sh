6b4b17dfaee619133ccb9b06f9690bc70c787f4a
OkYk <OkYk@github.com>
2016-03-26 06:17:49 -0400

    double input removed, non-looped video processing optimized

--- encode.sh
+++ encode.sh
@@ -1,7 +1,7 @@
 
 # clean it up from temporary files
 function clear_workspace {
-	rm -f video.txt audio.txt mylist.txt stream_video.mp4 audio_crop.mp3 output.mp4 ffmpeg2pass-0.log
+	rm -f video.txt audio.txt mylist.txt stream_video.mp4 output.mp4 ffmpeg2pass-0.log
 }
 
 # mask results of previous encode to avoid prompts for overwrite
@@ -39,19 +39,17 @@ scale=${scale:-$scale_def}
 read -p "Rate [$rate_def]:" rate
 rate=${rate:-$rate_def}
 
-read -e -p "" -i "${duration_def}" duration
-read -e -p "Video repeat:" -i "${scale_def}" scale
-read -e -p "Video repeat:" -i "${rate_def}" rate
-
 # creating the config for repeated video assembly and invoking it
-for i in $(seq 1 ${num}); do printf "file '%s'\n" video.mp4 >> mylist.txt; done
-ffmpeg -f concat -i mylist.txt -c copy stream_video.mp4 
+if [ "$num" -eq 1 ]
+then
+  cp video.mp4 stream_video.mp4
+else
+  for i in $(seq 1 ${num}); do printf "file '%s'\n" video.mp4 >> mylist.txt; done
+  ffmpeg -f concat -i mylist.txt -c copy stream_video.mp4 
+fi
 
-# cut the audio to have faster encode. otherwise the duration of temporary video might exceed the user-defined
-ffmpeg -ss 0 -t ${duration} -i audio.mp3 audio_crop.mp3
-
-# combining audio+video
-ffmpeg -i stream_video.mp4 -i audio_crop.mp3 -c:v copy -c:a aac -strict experimental -map 0:v:0 -map 1:a:0 output.mp4
+# combining audio+video cropping to user-defined length
+ffmpeg -ss 0 -t ${duration} -i stream_video.mp4 -i audio.mp3 -c:v copy -c:a aac -strict experimental -map 0:v:0 -map 1:a:0 output.mp4
 
 # 2-pass encoding with everything in place to res.webm
 ffmpeg -ss 0 -t ${duration} -i output.mp4 -codec:v libvpx -quality good -cpu-used 0 -b:v ${rate}k -maxrate ${rate}k -bufsize `expr ${rate} \* 2`k -qmin 10 -qmax 42 -vf scale=-1:${scale} -threads 4 -strict -2 -codec:a vorbis -b:a 128k -an -pass 1 -f webm -y /dev/null
