05c12c1540e8788d756171ac8eb8b4856ebeca75
OkYk <afalin@list.ru>
2016-06-03 23:31:00 -0400

    Latest mp3 and mp4 in the folder are used for encoding

--- encode.sh
+++ encode.sh
@@ -8,13 +8,16 @@ function clear_workspace {
 [ -e "res.webm" ] && mv res.webm res`ls -l | wc -l`.webm
 clear_workspace
 
+audio="`ls -1t *.mp3 | head -n1`"
+video="`ls -1t *.mp4 | head -n1`"
+
 # reads data on audio/video
-audio=`ffprobe.exe audio.mp3 2>&1 | tee audio.txt`
-video=`ffprobe.exe video.mp4 2>&1 | tee video.txt`
+audiodata=`ffprobe.exe "$audio" 2>&1 | tee audio.txt`
+videodata=`ffprobe.exe "$video" 2>&1 | tee video.txt`
 
 # extracts defaults
-audio_duration=`echo $audio | grep -oE "Duration: [^ ]+" | grep -oE "[0-9]+\.[0-9]+"`
-video_duration=`echo $video | grep -oE "Duration: [^ ]+" | grep -oE "[0-9]+\.[0-9]+"`
+audio_duration=`echo $audiodata | grep -oE "Duration: [^ ]+" | grep -oE "[0-9]+\.[0-9]+"`
+video_duration=`echo $videodata | grep -oE "Duration: [^ ]+" | grep -oE "[0-9]+\.[0-9]+"`
 
 num_def=1
 rate_def=1200
@@ -28,7 +31,7 @@ num=${num:-$num_def}
 
 # powershell for float math as I do have mingw, but bc not part of it yet
 duration_def=`powershell ${video_duration} \* ${num}`
-scale_def=`echo $video | grep -E "Stream" | grep -oE " [0-9]+x[0-9]+ "| grep -oE "x[0-9]+" | grep -oE "[0-9]+"`
+scale_def=`echo "$videodata" | grep -E "Stream" | grep -oE " [0-9]+x[0-9]+ "| grep -oE "x[0-9]+" | grep -oE "[0-9]+"`
 
 read -p "Duration [$duration_def]:" duration
 duration=${duration:-$duration_def}
@@ -42,14 +45,14 @@ rate=${rate:-$rate_def}
 # creating the config for repeated video assembly and invoking it
 if [ "$num" -eq 1 ]
 then
-  cp video.mp4 stream_video.mp4
+  cp "$video" stream_video.mp4
 else
-  for i in $(seq 1 ${num}); do printf "file '%s'\n" video.mp4 >> mylist.txt; done
+  for i in $(seq 1 ${num}); do printf "file '%s'\n" "$video" >> mylist.txt; done
   ffmpeg -f concat -i mylist.txt -c copy stream_video.mp4 
 fi
 
 # combining audio+video cropping to user-defined length
-ffmpeg -ss 0 -t ${duration} -i stream_video.mp4 -i audio.mp3 -c:v copy -c:a aac -strict experimental -map 0:v:0 -map 1:a:0 output.mp4
+ffmpeg -ss 0 -t ${duration} -i stream_video.mp4 -i "$audio" -c:v copy -c:a aac -strict experimental -map 0:v:0 -map 1:a:0 output.mp4
 
 # 2-pass encoding with everything in place to res.webm
 ffmpeg -ss 0 -t ${duration} -i output.mp4 -codec:v libvpx -quality good -cpu-used 0 -b:v ${rate}k -maxrate ${rate}k -bufsize `expr ${rate} \* 2`k -qmin 10 -qmax 42 -vf scale=-1:${scale} -threads 4 -strict -2 -codec:a vorbis -b:a 128k -an -pass 1 -f webm -y /dev/null
